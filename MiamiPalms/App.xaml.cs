d92e763ac316bb82b1ac491d6f127af37f5c5bf8
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-29 18:49:28 +0300

    Добавьте файлы проекта.

---
+++ MiamiPalms/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MiamiPalms.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MiamiPalms
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static MiamiPalmsDbEntities _context;
+        public static MiamiPalmsDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new MiamiPalmsDbEntities();
+            }
+            return _context;
+        }
+    }
+}
