9d560cd729c4c045c69ec93afcead5c850be2817
Влад Козлов <vkozlov15012005@gmail.com>
2025-06-05 22:51:03 +0300

    Добавлены ресурсы и обновлена страница DeskPage
    В проект MiamiPalms внесены следующие изменения:
    - В файл MiamiPalms.csproj добавлены ресурсы: pencil-icon.png, erase-icon.png и desk-picture.jpg.
    - В DeskPage.xaml увеличены размеры окна, добавлены элементы интерфейса: сетка, логотип, контактная информация, InkCanvas для рисования, иконки карандаша и ластика, кнопка "Назад".
    - В DeskPage.xaml.cs реализованы методы SetMarkerMode и SetEraserMode, добавлены обработчики событий для переключения режимов рисования/стирания и возврата в меню.
    - В пространстве имен MiamiPalms.View.Pages добавлены функции для взаимодействия с интерфейсом и навигации.

--- MiamiPalms/View/Pages/DeskPage.xaml.cs
+++ MiamiPalms/View/Pages/DeskPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiamiPalms.AppData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,44 @@ namespace MiamiPalms.View.Pages
     /// </summary>
     public partial class DeskPage : Page
     {
+    // Толщина маркера
         public DeskPage()
         {
             InitializeComponent();
+            SetMarkerMode();
+        }
+
+        private void SetMarkerMode()
+        {
+            DrawCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            DrawCanvas.DefaultDrawingAttributes = new System.Windows.Ink.DrawingAttributes
+            {
+                Color = Colors.Red,
+                Width = 4,
+                Height = 4,
+                FitToCurve = true
+            };
+        }
+
+        private void SetEraserMode()
+        {
+            DrawCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            DrawCanvas.EraserShape = new RectangleStylusShape(20, 20);
+        }
+
+        private void PencilIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            SetMarkerMode();
+        }
+
+        private void EraserIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            SetEraserMode();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FrameHelper.selectedFrame.Navigate(new MenuPage());
         }
     }
 }
